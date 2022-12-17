63ed5209a6aad86ec470dd7ed14333bd69bc41c5
Guilherme Gordiano <guilhermeg2004@gmail.com>
2022-12-17 17:47:09 -0300

    Versão 0.11 (Testes com o IdentityUser)

--- Schwarz/Areas/Identity/Data/SchwarzUser.cs
+++ Schwarz/Areas/Identity/Data/SchwarzUser.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Microsoft.AspNetCore.Identity;
+using Schwarz.Models;
 
 namespace Schwarz.Areas.Identity.Data;
 
-// Add profile data for application users by adding properties to the SchwarzUser class
 public class SchwarzUser : IdentityUser
 {
+	[Required(ErrorMessage = "Digite a matrícula")]
+	[Display(Name = "Matrícula")]
+	public int Matricula { get; set; }
+	[Required(ErrorMessage = "Digite o Nome do Funcionário")]
+	public string Nome { get; set; }
+	[Required(ErrorMessage = "Digite o Setor do Funcionário")]
+	public string Setor { get; set; }
+	[Required]
+	public bool Ativo { get; set; }
 }
+
 
---
+++ Schwarz/Models/EquipeIdeia.cs
@@ -0,0 +1,24 @@
+﻿
+using Schwarz.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Schwarz.Models
+{
+	public class EquipeIdeia
+	{
+		[Key]
+		public int IDEquipeIdeia { get; set; }
+		[ForeignKey("AspNetUser")]
+		public string IdUser { get; set; }
+		public virtual SchwarzUser User { get; set; }
+
+
+		EquipeIdeia() { }
+		public void GetTeste()
+		{
+			EquipeIdeia User = new();
+		}
+
+	}
+}
