b60822b223f72a3ca5b8f8cbbdc4861d266261e6
Guilherme Gordiano <guilhermeg2004@gmail.com>
2024-02-05 07:50:48 -0300

    Recursos Humanos - Criado Controller de Recursos Humanos - Criado View Ramais e Aniversariantes - Ocultado view Aniversariantes - Corrigido altura da página de login

---
+++ Schwarz/Controllers/RecursosHumanosController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Schwarz.Areas.Identity.Data;
+using Schwarz.Data;
+using Schwarz.Models;
+
+namespace Schwarz.Controllers
+{
+    public class RecursosHumanosController : Controller
+    {
+
+        private readonly SchwarzContext _context;
+        private readonly UserManager<SchwarzUser> _userManager;
+
+
+        public RecursosHumanosController(SchwarzContext context, UserManager<SchwarzUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public IActionResult Ramais()
+        {
+            List<Funcionario> ramais = _context.Funcionario.Where(x => x.Ativo && x.Ramal != null).Select(x => new Funcionario { Nome = x.Nome, Ramal = x.Ramal, Setor = x.Setor }).ToList();
+            return View(ramais);
+        }
+
+        public IActionResult Aniversariantes()
+        {
+            List<Funcionario> ramais = _context.Funcionario.Where(x => x.Ativo).Select(x => new Funcionario { Nome = x.Nome, DataNascimento = x.DataNascimento }).ToList();
+            return View(ramais);
+        }
+    }
+}
