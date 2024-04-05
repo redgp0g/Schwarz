b3a74f062a728418e216d6114545d450a14da458
Guilherme Gordiano <guilhermeg2004@gmail.com>
2024-04-05 11:14:33 -0300

    :sparkles: feat Arquivos e alterações Lições Aprendidas - Adicionado novos parametros para LicaoAprendida - Adicionado regra de Limitação para 9 digitos Código Interno - Adicionado ligação de Arquivos - Alterações Create, Edit e Index para mostrar novos campos - Removido Details

--- Schwarz/Models/LicaoAprendida.cs
+++ Schwarz/Models/LicaoAprendida.cs
@@ -9,8 +9,14 @@ namespace Schwarz.Models
         [Key]
         public int IDLicaoAprendida { get; set; }
 
-        [Required(ErrorMessage = "O Evento é obrigatório!")]
-        public string Evento { get; set; }
+        [Required(ErrorMessage = "O campo Planejado é obrigatório!")]
+        public string Planejado { get; set; }
+
+        [Required(ErrorMessage = "O campo Realizado é obrigatório!")]
+        public string Realizado { get; set; }
+
+        [Required(ErrorMessage = "O campo Motivo é obrigatório!")]
+        public string Motivo { get; set; }
 
         [ForeignKey("Funcionario")]
         public int IDFuncionario { get; set; }
@@ -27,10 +33,8 @@ namespace Schwarz.Models
 
         [Display(Name = "Código Interno")]
         [Required(ErrorMessage = "O Codigo Interno é obrigatório!")]
+        [Range(100000000,999999999,ErrorMessage = "O Código interno está errado!")]
         public int CodigoInterno { get; set; }
-
-        [Required(ErrorMessage = "O Motivo é obrigatório!")]
-        public string Motivo { get; set; }
 
         [Display(Name = "Lição Aprendida")]
         [Required(ErrorMessage = "A Lição Aprendida é obrigatória!")]
@@ -40,6 +44,9 @@ namespace Schwarz.Models
         public bool Positivo { get; set; }
 
         public virtual ICollection<LicaoAprendidaArquivo>? LicaoAprendidaArquivos { get; set; }
+
+        [NotMapped]
+        public List<IFormFile>? Arquivos { get; set; }
 
         public LicaoAprendida() { }
     }
