b5839699664152b22f4be7de3c914dfe0b92233a
Guilherme Gordiano <guilhermeg2004@gmail.com>
2024-04-18 11:37:00 -0300

    Atualização Entidade TransporteMercadoria - Removido propriedades que guardavam o caminho das fotos e substituido por uma ligação de 1 para muitos com uma tabela exclusiva para essas fotos - Criado nova entidade para guardar as fotos dos transportes de mercadorias

---
+++ Schwarz/Models/TransporteMercadoriaFoto.cs
@@ -0,0 +1,34 @@
+﻿using Schwarz.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Schwarz.Models
+{
+	public class TransporteMercadoriaFoto
+    {
+		[Key]
+		public int IDTransporteMercadoriaFoto { get; set; }
+
+		[ForeignKey("TransporteMercadoria")]
+        public int IDTransporteMercadoria { get; set; }
+        public virtual TransporteMercadoria TransporteMercadoria { get; set; }
+        public string Nome { get; set; }
+
+        [Column(TypeName = "varbinary(max)")]
+        public byte[] Conteudo{ get; set; }
+
+		private readonly SchwarzContext _context;
+	
+        public TransporteMercadoriaFoto()
+		{
+
+		}
+
+        public TransporteMercadoriaFoto(int iDTransporteMercadoria, string nome, byte[] conteudo)
+        {
+            IDTransporteMercadoria = iDTransporteMercadoria;
+            Nome = nome;
+            Conteudo = conteudo;
+        }
+    }
+}
