97be25d8cd738176c7ee854aba9b7220df749682
Guilherme Gordiano <guilhermeg2004@gmail.com>
2024-04-23 09:00:32 -0300

    :bricks: fix Salvar anexos direto no Banco de Dados - Removido Arquivo o qual serviria para centralizar todos os arquivos de todos os projetos, porque isso causaria problema de organização - Removido Ligação de LiçãoAprendida e Arquivo - Criado entidade específica para Anexos de LicaoAprendida - Atualizado contexto, removendo entidade antigas e adicionado nova

---
+++ Schwarz/Models/LicaoAprendidaAnexo.cs
@@ -0,0 +1,36 @@
+﻿using Schwarz.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Schwarz.Models
+{
+	public class LicaoAprendidaAnexo
+    {
+		[Key]
+		public int IDLicaoAprendidaAnexo { get; set; }
+
+		[ForeignKey("LicaoAprendida")]
+        public int IDLicaoAprendida{ get; set; }
+        public virtual LicaoAprendida LicaoAprendida { get; set; }
+        public string Nome { get; set; }
+
+        [Column(TypeName = "varbinary(max)")]
+        public byte[] Conteudo{ get; set; }
+        public string TipoMIME { get; set; }
+
+        private readonly SchwarzContext _context;
+	
+        public LicaoAprendidaAnexo()
+		{
+
+		}
+
+        public LicaoAprendidaAnexo(int iDLicaoAprendida, string nome, byte[] conteudo, string tipoMIME)
+        {
+            IDLicaoAprendida = iDLicaoAprendida;
+            Nome = nome;
+            Conteudo = conteudo;
+            TipoMIME = tipoMIME;
+        }
+    }
+}
