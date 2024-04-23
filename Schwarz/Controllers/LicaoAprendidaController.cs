daf60264c20814f9489e995f833eab5c829ec6f6
Guilherme Gordiano <guilhermeg2004@gmail.com>
2024-04-23 09:12:23 -0300

    :recycle: fix Alterações Controller novo LicaoAprendidaAnexo - Atualizado de LicaoAprendidaArquivos para LicaoAprendidaAnexos - Corrigido create LicaoAprendida - Corrigido delete LicaoAprendida

--- Schwarz/Controllers/LicaoAprendidaController.cs
+++ Schwarz/Controllers/LicaoAprendidaController.cs
@@ -23,7 +23,7 @@ namespace Schwarz.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var schwarzContext = _context.LicaoAprendida.Include(x => x.LicaoAprendidaArquivos);
+            var schwarzContext = _context.LicaoAprendida.Include(x => x.LicaoAprendidaAnexos);
             return View(await schwarzContext.ToListAsync());
         }
 
@@ -77,11 +77,8 @@ namespace Schwarz.Controllers
                                     string tipoMime = file.ContentType;
                                     string fileName = file.FileName;
 
-                                    Arquivo arquivo = new(fileName, fileBytes, tipoMime, DateTime.Now);
-                                    _context.Add(arquivo);
-                                    _context.SaveChanges();
-                                    ArquivoLicaoAprendida licaoAprendidaArquivo = new(licaoAprendida.IDLicaoAprendida, arquivo.IDArquivo);
-                                    _context.Add(licaoAprendidaArquivo);
+                                    LicaoAprendidaAnexo anexo = new(licaoAprendida.IDLicaoAprendida, fileName, fileBytes, tipoMime);
+                                    _context.Add(anexo);
                                     _context.SaveChanges();
                                 }
                             }
@@ -153,12 +150,11 @@ namespace Schwarz.Controllers
             {
                 return NotFound();
             }
-            if (licaoAprendida.LicaoAprendidaArquivos != null)
+            if (licaoAprendida.LicaoAprendidaAnexos != null)
             {
-                foreach (var licaoAprendidaArquivo in licaoAprendida.LicaoAprendidaArquivos)
+                foreach (var licaoAprendidaArquivo in licaoAprendida.LicaoAprendidaAnexos)
                 {
-                    _context.Arquivo.Remove(licaoAprendidaArquivo.Arquivo);
-                    _context.ArquivoLicaoAprendida.Remove(licaoAprendidaArquivo);
+                    _context.Remove(licaoAprendidaArquivo);
                 }
             }
 
