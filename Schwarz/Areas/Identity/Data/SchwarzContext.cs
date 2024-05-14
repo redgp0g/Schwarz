c36a7ec12f5febf591252f30ce63c9654961c4af
Guilherme Gordiano <guilhermeg2004@gmail.com>
2024-05-14 09:26:44 -0300

    :broom: Removendo código descontinuado

--- Schwarz/Areas/Identity/Data/SchwarzContext.cs
+++ Schwarz/Areas/Identity/Data/SchwarzContext.cs
@@ -12,25 +12,23 @@ public class SchwarzContext : IdentityDbContext<SchwarzUser>
         : base(options)
     {
     }
-    public DbSet<Cota> Cota{ get; set; }
 	public DbSet<EquipeIdeia> EquipeIdeia { get; set; }
     public DbSet<Falha> Falha { get; set; }
 	public DbSet<Funcionario> Funcionario { get; set; }
 	public DbSet<Ideia> Ideia { get; set; }
     public DbSet<IdeiaAnexo> IdeiaAnexo { get; set; }
-	public DbSet<PlanoControle> PlanoControle { get; set; }
     public DbSet<Registro> Registro { get; set; }
     public DbSet<RegistroCotas> RegistroCotas { get; set; }
     public DbSet<AlertaCota> AlertaCota { get; set; }
     public DbSet<LicaoAprendida> LicaoAprendida { get; set; }
     public DbSet<LicaoAprendidaAnexo> LicaoAprendidaAnexo { get; set; }
     public DbSet<Cliente> Cliente { get; set; }
+    public DbSet<TransporteMercadoria> TransporteMercadoria { get; set; }
+    public DbSet<CadastroPare> CadastroPare { get; set; }
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
     }
 
-public DbSet<TransporteMercadoria> TransporteMercadoria { get; set; }
-
-public DbSet<CadastroPare> CadastroPare { get; set; }
 }
