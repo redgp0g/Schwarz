434920b284396617419227ea0810e755809dbb2c
Guilherme Gordiano <guilhermeg2004@gmail.com>
2024-05-15 09:23:01 -0300

    :construction: Criado Classe VencedorPare

---
+++ Schwarz/Models/VencedorPare.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Schwarz.Models
+{
+	public class VencedorPare
+	{
+
+        [Key]
+        public int IDVencedorPare { get; set; }
+
+        [DisplayName("Pontuação Total")]
+        public int PontuacaoTotal { get; set; }
+        public int Mes { get; set; }
+        public int Ano { get; set; }
+    }
+}
