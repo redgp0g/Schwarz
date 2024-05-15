328b387ec1f6af388848124a3b471b4dd2be23a1
Guilherme Gordiano <guilhermeg2004@gmail.com>
2024-05-15 11:31:27 -0300

    :sparkles: feat Máscara de Ordem de Serviço

--- Schwarz/wwwroot/js/site.js
+++ Schwarz/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-function ShowLoading() {
+﻿function ShowLoading() {
     const divloading = document.createElement("div");
     const divbox = document.createElement("div");
     const label = document.createElement("label");
@@ -26,6 +26,22 @@ function HideLoading() {
         loadings[0].remove();
     }
 }
+$('.ordemServico').on('input', function () {
+    var num = $(this).val().replace(/\D/g, '');
+
+    if (num.length > 11) {
+        num = num.substring(0, 11);
+    }
+
+    var formattedNum = '';
+    for (var i = 0; i < num.length; i++) {
+        if (i === 3 || i === 6) {
+            formattedNum += '.';
+        }
+        formattedNum += num[i];
+    }
+    $(this).val(formattedNum);
+});
 
 $('.select2').select2({
     width: '100%',
