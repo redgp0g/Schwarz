8c098663c2058de92132e13f346d5f4684fc2021
Guilherme Gordiano <guilhermeg2004@gmail.com>
2024-05-28 21:13:59 -0300

    Inicialização de Funcionario

--- Schwarz/Areas/Identity/Data/SchwarzUser.cs
+++ Schwarz/Areas/Identity/Data/SchwarzUser.cs
@@ -15,8 +15,8 @@ namespace Schwarz.Areas.Identity.Data;
 public class SchwarzUser : IdentityUser
 {
     [ForeignKey("Funcionario")]
-    public int IDFuncionario{ get; set; }  
-    public virtual Funcionario? Funcionario { get; set; }
+    public int IDFuncionario{ get; set; }
+    public virtual Funcionario Funcionario { get; set; } = new Funcionario();
 
 }
 
