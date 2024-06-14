83abe7193266c4b567e1950074df692dd9c69709
Guilherme Gordiano <guilhermeg2004@gmail.com>
2024-06-14 08:51:47 -0300

    :sparkles: feat Criar EmailService - Criar função sendEmail para enviar email com

---
+++ Schwarz/Services/EmailService.cs
@@ -0,0 +1,34 @@
+﻿using Schwarz.Services.Interfaces;
+using System.Net.Mail;
+using System.Net;
+
+namespace Schwarz.Services
+{
+    public class EmailService : IEmailService
+    {
+
+        public void SendEmail(string subject, string message, string email)
+        {
+            MailMessage emailMessage = new();
+
+            var emailComunicacao = Environment.GetEnvironmentVariable("SCHWARZ_EMAIL_COMUNICACAO");
+            var senhaEmailComunicacao= Environment.GetEnvironmentVariable("SCHWARZ_SENHA_EMAIL_COMUNICACAO");
+
+            var smtpClient = new SmtpClient("smtp.gmail.com", 587)
+            {
+                EnableSsl = true,
+                Timeout = 60000 * 6000,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(emailComunicacao, senhaEmailComunicacao)
+            };
+            emailMessage.From = new MailAddress(emailComunicacao, "Schwarz Comunicação");
+
+            emailMessage.Body = message;
+            emailMessage.Subject = subject;
+            emailMessage.IsBodyHtml = true;
+            emailMessage.Priority = MailPriority.Normal;
+            emailMessage.To.Add(email);
+            smtpClient.Send(emailMessage);
+        }
+    }
+}
---
+++ Schwarz/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Net.Mail;
+using System.Net;
+
+namespace Schwarz.Services.Interfaces
+{
+    public interface IEmailService
+    {
+
+        public void SendEmail(string subject, string message,string email);
+
+    }
+}
