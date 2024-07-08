2e41147c53731f95b1d0e18c71f0457865487869
Guilherme Gordiano <guilhermeg2004@gmail.com>
2024-07-08 13:46:30 -0300

    Configurar Api PareSeguranca - Adicionar serviço de Email - Atualizar rotas - Adicionar função de Plano de Ação para definir a ação do líder, prazo e se a ação foi realizada e função de Concluir para atualizar se a ação foi realizada e função de Validare Invalidar para a segurança validar a ação

--- Schwarz/Controllers/Api/PareSegurancaController.cs
+++ Schwarz/Controllers/Api/PareSegurancaController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Schwarz.Data;
 using Schwarz.Enums;
 using Schwarz.Models;
+using Schwarz.Services.Interfaces;
 
 namespace Schwarz.Controllers.Api
 {
@@ -14,12 +15,14 @@ namespace Schwarz.Controllers.Api
     public class PareSegurancaController : ControllerBase
     {
         private readonly SchwarzContext _context;
-        public PareSegurancaController(SchwarzContext schwarzContext)
+        private readonly IEmailService _emailService;
+        public PareSegurancaController(SchwarzContext schwarzContext, IEmailService emailService)
         {
             _context = schwarzContext;
+            _emailService = emailService;
         }
 
-        [HttpDelete("/{id}")]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
             var pareSeguranca = await _context.PareSeguranca.FindAsync(id);
@@ -40,7 +43,7 @@ namespace Schwarz.Controllers.Api
             return NotFound();
         }
 
-        [HttpDelete("/Foto/{id}")]
+        [HttpDelete("Foto/{id}")]
         public IActionResult DeleteFoto(int id)
         {
             var foto = _context.PareSegurancaFoto.Find(id);
@@ -53,7 +56,7 @@ namespace Schwarz.Controllers.Api
             return NotFound();
         }
 
-        [HttpPost("/Foto")]
+        [HttpPost("Foto")]
         public IActionResult AdicionarFoto(IFormFileCollection fotos, int idPareSeguranca)
         {
 
@@ -75,37 +78,78 @@ namespace Schwarz.Controllers.Api
             return Ok();
         }
 
-        [HttpPut("/AprovacaoLider")]
-        public async Task<IActionResult> AprovacaoLider(int idPareSeguranca, EAprovacaoPare aprovacaoPare, string? observacoes = null)
+        [HttpPut("PlanoAcao")]
+        public async Task<IActionResult> PlanoAcao([FromForm] int id, [FromForm] string acao, [FromForm] bool realizada = false, [FromForm] DateTime? prazoAcao = null)
         {
-            var pareSeguranca = await _context.PareSeguranca.FindAsync(idPareSeguranca);
-            if(pareSeguranca == null)
+            var pareSeguranca = await _context.PareSeguranca.FindAsync(id);
+            if (pareSeguranca == null)
             {
                 return NotFound();
             }
-            pareSeguranca.AprovacaoLider = aprovacaoPare;
-            pareSeguranca.ObservacoesLider = observacoes;
+
+            pareSeguranca.Realizado = realizada;
+            pareSeguranca.AcaoLider = acao;
+            pareSeguranca.PrazoAcaoLider = prazoAcao;
+            _context.Update(pareSeguranca);
+            _context.SaveChanges();
+            string emailMessage = $"Foi criada uma ação para o PARE por {pareSeguranca.Funcionario.FuncionarioLider.Nome} cuja descrição é: {acao} <br/>" + "Link do site:  <a href =\"http://192.168.2.96:5242/Pare/IndexSeguranca\">Sistema Integrado</a>";
+            string subject = "Ação do Líder para PARE de Segurança";
+            List<string> emails = _context.Funcionario.Where(x => x.Ativo).Where(x => x.Setor == "Segurança do Trabalho").Select(x => x.Email).ToList();
+            foreach (var email in emails)
+            {
+                //_emailService.SendEmail(subject, emailMessage, email);
+            }
+            _emailService.SendEmail(subject, emailMessage, "guilherme.gordiano@schwarz.com.br");
+
+            return Ok();
+        }
+
+        [HttpPut("Concluir")]
+        public async Task<IActionResult> Concluir([FromForm] int id)
+        {
+            var pareSeguranca = await _context.PareSeguranca.FindAsync(id);
+            if (pareSeguranca == null)
+            {
+                return NotFound();
+            }
+            pareSeguranca.Realizado = true;
             _context.Update(pareSeguranca);
             _context.SaveChanges();
 
             return Ok();
         }
-        
-        [HttpPut("/AprovacaoSeguranca")]
-        public async Task<IActionResult> AprovacaoSeguranca(int idPareSeguranca, EAprovacaoPare aprovacaoPare, string? observacoes = null)
+
+        [HttpPut("Validar")]
+        public async Task<IActionResult> Validar([FromForm] int id, [FromForm] string? observacoes = null)
         {
-            var pareSeguranca = await _context.PareSeguranca.FindAsync(idPareSeguranca);
-            if(pareSeguranca == null)
+            var pareSeguranca = await _context.PareSeguranca.FindAsync(id);
+            if (pareSeguranca == null)
             {
                 return NotFound();
             }
-            pareSeguranca.AprovacaoSeguranca = aprovacaoPare;
             pareSeguranca.ObservacoesSeguranca = observacoes;
+            pareSeguranca.DataValidado = DateTime.Now;
             _context.Update(pareSeguranca);
             _context.SaveChanges();
 
             return Ok();
         }
-       
+
+        [HttpPut("Invalidar")]
+        public async Task<IActionResult> Invalidar([FromForm] int id,[FromForm] string? observacoes = null)
+        {
+            var pareSeguranca = await _context.PareSeguranca.FindAsync(id);
+            if (pareSeguranca == null)
+            {
+                return NotFound();
+            }
+            pareSeguranca.ObservacoesSeguranca = observacoes;
+            pareSeguranca.Realizado = false;
+            _context.Update(pareSeguranca);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
     }
 }
