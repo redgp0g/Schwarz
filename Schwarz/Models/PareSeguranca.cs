cdb139d40514eb820286a2780701cf5cfab0b2ae
Guilherme Gordiano <guilhermeg2004@gmail.com>
2024-07-08 13:40:35 -0300

    Modificar Model PareSeguranca - Reordenar campos de Pontuacao e PontuacaoValida para o final - Adicionar Funcionário Líder, Ação do Líder, Prazo da Ação do Líder, Funcionário Segurança, Data de Validação da Ação e se foi Realizado

--- Schwarz/Models/PareSeguranca.cs
+++ Schwarz/Models/PareSeguranca.cs
@@ -1,11 +1,11 @@
 using Schwarz.Enums;
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Schwarz.Models
 {
-	public class PareSeguranca
-	{
+    public class PareSeguranca
+    {
         [Key]
         public int IDPareSeguranca { get; set; }
         [ForeignKey("Funcionario")]
@@ -23,28 +23,35 @@ namespace Schwarz.Models
         [Required(ErrorMessage = "O Desvio é obrigatório!")]
         public string Desvio { get; set; }
 
-        [Display(Name = "Pontuação")]
-        public int? Pontuacao { get; set; }
-        public bool PontuacaoValida { get; set; } = true;
-
         [Display(Name = "Ordem de Serviço")]
         [Required(ErrorMessage = "A Ordem de Serviço é obrigatória!")]
         public string OrdemServico { get; set; }
 
-        [Display(Name = "Aprovação do Líder")]
-        public EAprovacaoPare AprovacaoLider { get; set; } = EAprovacaoPare.SemAprovacao;
-        public DateTime? DataAprovacaoLider { get; set; }
+        [ForeignKey("FuncionarioLider")]
+        public int IDFuncionarioLider { get; set; }
+        public virtual Funcionario? FuncionarioLider { get; set; }
 
-        [Display(Name = "Observações do Líder")]
-        public string? ObservacoesLider { get; set; }
+        [Display(Name = "Ação do Líder")]
+        public string? AcaoLider { get; set; }
 
-        [Display(Name = "Aprovação da Segurança")]
-        public EAprovacaoPare AprovacaoSeguranca { get; set; } = EAprovacaoPare.SemAprovacao;
-        public DateTime? DataAprovacaoSeguranca { get; set; }
+        [Display(Name = "Prazo da Ação do Líder")]
+        public DateTime? PrazoAcaoLider { get; set; }
 
-        [Display(Name = "Observações do Segurança")]
+        [Display(Name = "Foi Realizado?")]
+        public bool Realizado { get; set; } = false;
+
+        [ForeignKey("FuncionarioSeguranca")]
+        public int? IDFuncionarioSeguranca { get; set; }
+        public virtual Funcionario? FuncionarioSeguranca { get; set; }
+
+        [Display(Name = "Observações da Segurança")]
         public string? ObservacoesSeguranca { get; set; }
+        public DateTime? DataValidado { get; set; }
         public int? ClassificacaoGUT { get; set; }
+
+        [Display(Name = "Pontuação")]
+        public int? Pontuacao { get; set; }
+        public bool PontuacaoValida { get; set; } = true;
         public virtual ICollection<PareSegurancaFoto>? PareSegurancaFotos { get; set; }
 
         [NotMapped]
